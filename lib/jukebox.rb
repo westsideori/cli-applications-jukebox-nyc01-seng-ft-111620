6685cd8532e6a84b95384c2d5dfeda9078f9d893
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-16 00:49:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,7 +36,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.strip
-  if choice.to_i <= 9 && choice >= 1
+  if choice.to_i <= 9 && choice.to_i >= 1
     puts "Playing #{songs[choice - 1]}"
   elsif songs.include?(choice)
     puts "Playing #{choice}"
